10d200bef24ed87f54e4db0e17bec0e0b6d7b7b2
Andy How Hok Hium <ahowh089@uottawa.ca>
2023-11-18 22:11:10 -0500

    add add/remove account from database unit testing

--- android studio project/app/src/test/java/com/example/servicenovigrad/ExampleUnitTest.java
+++ android studio project/app/src/test/java/com/example/servicenovigrad/ExampleUnitTest.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.util.Log;
+
+import com.example.servicenovigrad.backend.DatabaseHandler;
+import com.example.servicenovigrad.backend.account.Account;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.PrintStream;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +23,24 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void account_database_test(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String username = "admin";
+        String root = "users/" + username + "/";
+        DatabaseReference acctRef = database.getReference(root);
+        Account acct = new Account(username, "nom", "prenom", "Client", "password");
+        acctRef.setValue(acct);} //regarder dans la base de donnés si le nouveau compte a été ajouté
+    @Test
+    public void delete_Account(){;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String username = "admin";
+        String root = "users/" + username + "/";
+        DatabaseReference acctRef = database.getReference(root);
+        Account acct = new Account(username, "nom", "prenom", "Client", "password");
+        DatabaseHandler.deleteUser(acct); // regarder dans la base de donné si le nouveau compte a été enlevé (suppose que account_database_test() fonctionne)
+    }
+
+
 }

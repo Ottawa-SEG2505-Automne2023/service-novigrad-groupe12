372c18bb25236ad1c2b5c72e621612c30ba8786b
Javisen-Pillay <114540147+Javisen-Pillay@users.noreply.github.com>
2023-12-03 22:51:49 -0500

    Update MainActivity.java
    client search algorithm

--- android studio project/app/src/main/java/com/example/servicenovigrad/ui/MainActivity.java
+++ android studio project/app/src/main/java/com/example/servicenovigrad/ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.servicenovigrad.ui;
 
 import android.os.Bundle;
-
 import com.example.servicenovigrad.R;
+import com.example.servicenovigrad.backend.account.CompleteBranch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends WelcomeActivity {
+
+    private List<CompleteBranch> completeBranches = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,21 @@ public class MainActivity extends WelcomeActivity {
         setContentView(R.layout.activity_main);
 
         welcomeText = findViewById(R.id.mainWelcomeText);
+        List<CompleteBranch> filteredBranches = searchBranchByRating(3.5); // Replace 3.5 with your desired minimum rating
+    }
+
+    private List<CompleteBranch> searchBranchByRating(double minRating) {
+        List<CompleteBranch> filteredBranches = new ArrayList<>();
+
+        for (CompleteBranch branch : completeBranches) {
+            double rating = branch.rating();
+
+            // Handle the special case when there are currently no ratings (-1)
+            if (rating != -1 && rating >= minRating) {
+                filteredBranches.add(branch);
+            }
+        }
+
+        return filteredBranches;
     }
 }
